e25e39be3b0ce369d80ba20817d6dd0c09854036
bhanu-prasad-22 <bhanuprasadvuggam333@gmail.com>
2025-11-24 19:09:28 +0530

    Update CountXor.java

--- Day12/CountXor.java
+++ Day12/CountXor.java
@@ -1,8 +1,7 @@
-
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 class CountXor {
-
     static int CountXOR(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
@@ -11,22 +10,15 @@ class CountXor {
             prefix ^= num;
             int need = prefix ^ k;
             result += map.getOrDefault(need, 0);
-
-            map.put(need, map.getOrDefault(need, 0) + 1);
+            // record the current prefix XOR (not 'need')
+            map.put(prefix, map.getOrDefault(prefix, 0) + 1);
         }
-
         return result;
-
     }
 
     public static void main(String[] args) {
         int[] nums = {4, 2, 2, 6, 4};
         int k = 6;
-        System.out.println("answer = " + CountXOR(nums, k));
-
+        System.out.println("answer = " + CountXOR(nums, k)); // expected: answer = 4
     }
 }
-
-output:
-java CountXor.java
-answer = 4
